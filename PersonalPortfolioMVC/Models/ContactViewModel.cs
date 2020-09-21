87ca28c21751468415230053410b172e1afea340
Rachel <rpunches@gmail.com>
2020-09-21 18:00:31 -0500

    transfer code to github

---
+++ PersonalPortfolioMVC/Models/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalPortfolioMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Message is Required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
